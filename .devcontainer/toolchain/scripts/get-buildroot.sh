315132f77dd87a2a8784472468ce65b22215ef58
Bastien Veuthey <veutheybastien@gmail.com>
2023-11-11 12:18:08 +0100

    labo sshd

--- .devcontainer/toolchain/scripts/get-buildroot.sh
+++ .devcontainer/toolchain/scripts/get-buildroot.sh
@@ -12,5 +12,6 @@ git checkout -b ses 2022.08.3
 
 rsync -a /workspace/config/board/ /buildroot/board/
 rsync -a /workspace/config/configs/ /buildroot/configs/
+rsync -a /workspace/config/package/ /buildroot/package/
 
 make ses_defconfig

b325869c20f1d9ff875f142adb297511f60566cf
Bastien Veuthey <veutheybastien@gmail.com>
2023-11-23 23:19:39 +0100

    lab fs: ex3 & 4

--- config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 BUILDROOT_DIR="/buildroot"
 BOARD_DIR=$BUILDROOT_DIR/"$(dirname $0)"
+IMAGES_DIR=$BUILDROOT_DIR/output/images
+DEVICE=rootfs.lukfs
 
-cd $BUILDROOT_DIR/output/images
+cd $BOARD_DIR
+./initramfs.sh
+
+apt install cryptsetup -y
+dd if=/dev/urandom of=passphrase bs=64 count=1
+dd if=/dev/zero of=rootfs.lukfs bs=2147483648 count=1
+
+cryptsetup --debug --pbkdf pbkdf2 --key-file passphrase --batch-mode luksFormat $DEVICE
+cryptsetup --debug open --type luks --key-file passphrase $DEVICE usrfs1
+
+mkfs.ext4 /dev/mapper/usrfs1
+mkdir /mnt/lukfs
+mkdir /mnt/rootfs
+
+mount /dev/mapper/usrfs1 /mnt/lukfs
+mount $IMAGES_DIR/rootfs.ext4 /mnt/rootfs
+cp -pr /mnt/rootfs/* /mnt/lukfs/
+
+umount $IMAGES_DIR/rootfs.ext4
+umount /dev/mapper/usrfs1
+cryptsetup close usrfs1
+rm /mnt/lukfs
+rm /mnt/rootfs
+
+mv rootfs.lukfs $IMAGES_DIR/.
+mv passphrase $BUILDROOT_DIR/output/target/root/.
+
+cd $IMAGES_DIR
 
 #install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd boot.scr

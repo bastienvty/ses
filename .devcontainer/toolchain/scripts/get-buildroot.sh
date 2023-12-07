02944f5fda45059f1ed3078f5b798065d3ba4911
Bastien Veuthey <veutheybastien@gmail.com>
2023-12-07 12:35:39 +0100

    buildroot script makefile

--- .devcontainer/toolchain/scripts/get-buildroot.sh
+++ .devcontainer/toolchain/scripts/get-buildroot.sh
@@ -10,6 +10,7 @@ git clone git://git.buildroot.net/buildroot /buildroot
 cd /buildroot
 git checkout -b ses 2022.08.3
 
+rsync -a /workspace/config/Makefile /buildroot/Makefile
 rsync -a /workspace/config/board/ /buildroot/board/
 rsync -a /workspace/config/configs/ /buildroot/configs/
 rsync -a /workspace/config/package/ /buildroot/package/
